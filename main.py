5134f0ed9f0861ed960ec7854e93dec4f2e38885
Shady Azouz <sazouz@sumerge.com>
2023-01-12 14:55:56 +0200

    initialize project

--- main.py
+++ main.py
@@ -40,21 +40,73 @@ def printDict(myDict, microFrontend):
             if checkDuplicate(myDict[i]):
                 addDuplicate(myDict[i], i, microFrontend)
             else:
-                list.append([i, myDict[i]])
+                list.append([microFrontend+"."+i, myDict[i]])
 
 
 for i in data:
     print(i)
     printDict(data[i], i)
 
-with open("output_Duplicates.csv", 'w', encoding='utf-8-sig', newline="") as csvfile:
-    csvWriter = csv.writer(csvfile)
-    for i in duplicates:
-        csvWriter.writerow(i)
-    print(duplicatesNum)
-    print(len(duplicates))
-    
+# with open("Arabic_Duplicates.csv", 'w', encoding='utf-8-sig', newline="") as csvfile:
+#     csvWriter = csv.writer(csvfile)
+#     for i in duplicates:
+#         csvWriter.writerow(i)
+#     print(duplicatesNum)
+#     print(len(duplicates))
 
+with open("Arabic_Unique.csv", 'w', encoding='utf-8-sig', newline="") as csvfile:
+    csvWriter = csv.writer(csvfile)
+    for i in list:
+        csvWriter.writerow(i)
+
+def checkDuplicateList(ele):
+    for i in duplicates:
+        if i[0] == ele:
+            return True
+    return False
+
+uniqueValues = []
+otherDuplicates = []
+
+def checkOtherDuplicaes(ele):
+    for i in otherDuplicates:
+        if i[0] == ele:
+            return True
+    return False
+
+def addOtherDuplicate(ele, key):
+    for i in otherDuplicates:
+        if i[0] == ele:
+            i.append(key)
+            return
+    otherDuplicates.append([ele, key])
+
+def removeDuplicates(myDict):
+    global uniqueValues
+    for i in myDict:
+        if type(myDict[i]) == dict:
+            return removeDuplicates(myDict[i])
+        else:
+            if checkDuplicateList(myDict[i]):
+                # if not checkOtherDuplicaes(myDict[i]):
+                #     uniqueValues.append([i, myDict[i]])
+                addOtherDuplicate(myDict[i], i)
+            else:
+                uniqueValues.append([i, myDict[i]])
+for i in data:
+    uniqueValues = []
+    otherDuplicates = []
+    removeDuplicates(data[i])
+    fileName = '%s_unique_en.csv' % i
+    # with open(fileName, 'w', encoding='utf-8-sig', newline="") as csvfile:
+    #     csvWriter = csv.writer(csvfile)
+    #     for x in uniqueValues:
+    #         csvWriter.writerow(x)
+    # fileName = '%s_duplicates_en.csv' % i
+    # with open(fileName, 'w', encoding='utf-8-sig', newline="") as csvfile:
+    #     csvWriter = csv.writer(csvfile)
+    #     for i in otherDuplicates:
+    #         csvWriter.writerow(i)
 
 
 f.close()
